3bf6b199ff854a21bca353b7ed08a35cd1ad6585
David Lima <antdavidlima@gmail.com>
2022-08-02 21:08:34 -0300

    chore: inicialize project

---
+++ src/main/java/com/davidlima/trixlogtechnicalchallenge/TrixlogTechnicalChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.davidlima.trixlogtechnicalchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrixlogTechnicalChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrixlogTechnicalChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davidlima/trixlogtechnicalchallenge/TrixlogTechnicalChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidlima.trixlogtechnicalchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrixlogTechnicalChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
